150ad1e884d7272a60d589c8985df624a25d27fa
wdunlap@2ndwatch.com <wdunlap@2ndwatch.com>
2023-03-13 22:13:45 +0000

    updates

--- macros/generate_schema_name.sql
+++ macros/generate_schema_name.sql
@@ -1,10 +1,11 @@
--- target names:
--- cloud
---      default (cloud 'feature' development environment)
---      dev (cloud 'development' deployment environment)
---      prod (cloud 'production' deployment environment)
--- .github/profiles.yml
---      prod (web_app profile)
+-- this macro overrides DBT's default logic for schema naming in snowflake
+-- with a couple updates to dev and prod build jobs in the cloud ide, this macro will drop the prefix from the dev and prod environments
+-- so, for example, rather than 'DEV_DW' in DB_ANALYTICS_DEV and 'FBT_DW' in DB_ANALYTICS_PROD, both databases will contain have the schema 'DW'
+-- one reason you might consider doing this is to simplify parameterizing in PBI workspaces and reports (if you make this change, all report parameters will need to be updated)
+
+-- to make this change:
+-- edit the 'Build Dev Daily 12AM ET' job in the cloud IDE: update the target name to 'development'
+-- and edit the 'Build Prod Daily 1AM ET' job in the cloud IDE: update the target name to 'production'
 
 {% macro generate_schema_name(custom_schema_name, node) -%}
 
