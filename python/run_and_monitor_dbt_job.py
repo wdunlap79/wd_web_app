c39db726fddf1625ed303c29d464a405834f16ec
wdunlap@2ndwatch.com <wdunlap@2ndwatch.com>
2023-03-16 21:37:41 +0000

    updates

--- python/run_and_monitor_dbt_job.py
+++ python/run_and_monitor_dbt_job.py
@@ -34,6 +34,7 @@ job_id: {job_id}
 # use environment variables to set configuration
 #------------------------------------------------------------------------------
 req_auth_header = {'Authorization': f'Token {api_key}'}
+
 req_job_url = f'{api_base}/api/v2/accounts/{account_id}/jobs/{job_id}/run/'
 run_status_map = { # dbt run statuses are encoded as integers. This map provides a human-readable status
   1:  'Queued',
